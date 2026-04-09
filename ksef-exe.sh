ed0436f63bbe42558ca011e0b288eb9d86e6d274
m32 <grzegorz.makarewicz@gmail.com>
2026-04-09 22:35:22 +0200

    generacja htmli lub pdfów

--- ksef-exe.sh
+++ ksef-exe.sh
@@ -3,12 +3,27 @@
 ./KSeF-PDFGen \
 -t invoice \
 -i assets/invoice.xml \
--o ksef-exe-fa.pdf \
 --nrKSeF "1111111111-20251107-080080679C57-14" \
 --qrCode "https://qr.ksef.mf.gov.pl/invoice/{nip}/{p1}/{hash}" \
---qr2Code "https://qr.ksef.mf.gov.pl/certificate/Nip/1111111111/{nip}/01F20A5D352AE590/..."
+--qr2Code "https://qr.ksef.mf.gov.pl/certificate/Nip/1111111111/{nip}/01F20A5D352AE590/..." \
+-o ksef-exe-fa.pdf
+
+./KSeF-PDFGen \
+-t invoice \
+-i assets/invoice.xml \
+--nrKSeF "1111111111-20251107-080080679C57-14" \
+--qrCode "https://qr.ksef.mf.gov.pl/invoice/{nip}/{p1}/{hash}" \
+--qr2Code "https://qr.ksef.mf.gov.pl/certificate/Nip/1111111111/{nip}/01F20A5D352AE590/..." \
+-o ksef-exe-fa.html \
+--html
 
 ./KSeF-PDFGen \
 -t upo \
 -i assets/upo.xml \
 -o ksef-exe-upo.pdf
+
+./KSeF-PDFGen \
+-t upo \
+-i assets/upo.xml \
+-o ksef-exe-upo.html \
+--html
