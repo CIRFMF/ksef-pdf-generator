e73ed7db4dbff2979a5b398f17cdbb187feb0489
m32 <grzegorz.makarewicz@gmail.com>
2026-05-08 12:00:40 +0200

    online/offline

--- ksef-exe.sh
+++ ksef-exe.sh
@@ -5,8 +5,7 @@
 -i assets/invoice.xml \
 --nrKSeF "1111111111-20251107-080080679C57-14" \
 --qrCode "https://qr.ksef.mf.gov.pl/invoice/{nip}/{p1}/{hash}" \
---qr2Code "https://qr.ksef.mf.gov.pl/certificate/Nip/1111111111/{nip}/01F20A5D352AE590/..." \
--o ksef-exe-fa.pdf
+-o ksef-exe-fa-online.pdf
 
 ./KSeF-PDFGen \
 -t invoice \
@@ -14,7 +13,23 @@
 --nrKSeF "1111111111-20251107-080080679C57-14" \
 --qrCode "https://qr.ksef.mf.gov.pl/invoice/{nip}/{p1}/{hash}" \
 --qr2Code "https://qr.ksef.mf.gov.pl/certificate/Nip/1111111111/{nip}/01F20A5D352AE590/..." \
--o ksef-exe-fa.html \
+-o ksef-exe-fa-offline.pdf
+
+./KSeF-PDFGen \
+-t invoice \
+-i assets/invoice.xml \
+--nrKSeF "1111111111-20251107-080080679C57-14" \
+--qrCode "https://qr.ksef.mf.gov.pl/invoice/{nip}/{p1}/{hash}" \
+-o ksef-exe-fa-online.html \
+--html
+
+./KSeF-PDFGen \
+-t invoice \
+-i assets/invoice.xml \
+--nrKSeF "1111111111-20251107-080080679C57-14" \
+--qrCode "https://qr.ksef.mf.gov.pl/invoice/{nip}/{p1}/{hash}" \
+--qr2Code "https://qr.ksef.mf.gov.pl/certificate/Nip/1111111111/{nip}/01F20A5D352AE590/..." \
+-o ksef-exe-fa-offline.html \
 --html
 
 ./KSeF-PDFGen \
